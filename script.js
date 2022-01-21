5461e8a23e54460027a32dc55c361385227da470
Fabi0o <marfab32@gmail.com>
2022-01-21 17:07:52 +0100

    creat function that displays books

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+let library = document.querySelector(".library");
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -9,4 +10,15 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+}
+function displayBooks() {
+  myLibrary.forEach((book) => {
+    library.innerHTML += `<div class="book">
+        <div class="title, bookItem">${book.title}</div>
+        <div class="author, bookItem">${book.author}</div>
+        <div class="pages, bookItem">${book.pages}</div>
+        <button class="read, bookItem">${book.read}</button>
+        <button class="remove, bookItem">Remove</button>
+      </div>`;
+  });
 }
