67083018970bc8561f1acf707da7aeb81bee9589
Fabi0o <marfab32@gmail.com>
2022-01-22 18:52:47 +0100

    creat function that makes reamove button to work

--- script.js
+++ script.js
@@ -21,14 +21,18 @@ function addBookToLibrary(book) {
 }
 function displayBooks() {
   myLibrary.forEach((book) => {
-    library.innerHTML += `<div class="book">
-        <div class="title, bookItem">"${book.title}"</div>
-        <div class="author, bookItem">${book.author}</div>
-        <div class="pages, bookItem">${book.pages}</div>
-        <button class="read, bookItem">${book.read}</button>
-        <button class="remove, bookItem">Remove</button>
+    library.innerHTML += `<div class="book" data-index-number = ${myLibrary.indexOf(
+      book
+    )}>
+        <div class="title bookItem">"${book.title}"</div>
+        <div class="author bookItem">${book.author}</div>
+        <div class="pages bookItem">${book.pages}</div>
+        <button class="read bookItem">${book.read}</button>
+        <button class="remove bookItem">Remove</button>
       </div>`;
   });
+  readButtonFunctionality();
+  ramoveButtonFunctionality();
 }
 displayBooks();
 //takes value from the form and creates books accordingly
@@ -43,7 +47,7 @@ btn.addEventListener("click", function (e) {
   let read = "Read";
   if (title != "" && author != "" && pages != 0) {
     if (!isRead) {
-      read = "Not Read";
+      read = "Not read";
     }
     let newBook = new Book(title, author, pages, read);
     library.innerHTML = "";
@@ -51,3 +55,34 @@ btn.addEventListener("click", function (e) {
     displayBooks();
   } else alert("Every form input have to be filled!");
 });
+//makes read button to work
+function readButtonFunctionality() {
+  const buttons = document.querySelectorAll(".read");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      if (button.textContent == "Read") {
+        button.textContent = "Not read";
+        let parent = button.parentElement;
+        let arrayItem = myLibrary[parent.dataset.indexNumber];
+        arrayItem.read = "Not read";
+      } else if (button.textContent == "Not read") {
+        button.textContent = "Read";
+        let parent = button.parentElement;
+        let arrayItem = myLibrary[parent.dataset.indexNumber];
+        arrayItem.read = "Read";
+      }
+    });
+  });
+}
+//makes remove button to work
+function ramoveButtonFunctionality() {
+  const buttons = document.querySelectorAll(".remove");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      let parent = button.parentElement;
+      myLibrary.splice(parent.dataset.indexNumber, 1);
+      library.innerHTML = "";
+      displayBooks();
+    });
+  });
+}
